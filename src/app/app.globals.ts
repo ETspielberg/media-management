acf93ab79410f2cb3dd2f7ed8acfb60bc013d9b8
Eike Spielberg <Eike.Spielberg@uni-due.de>
2020-09-07 17:49:32 +0200

    changed path to linksolver

--- src/app/app.globals.ts
+++ src/app/app.globals.ts
@@ -4,5 +4,5 @@ export const resourcesUrl = '/api/resources';
 export const counterUrl = '/api/counterretrieval';
 export const filesUrl = '/files';
 export const counterretrievalUrl = '/api/counterretrieval';
-export const shibbolethDataUrl = '/api/settings/shibbolethData';
+export const shibbolethDataUrl = '/service/linksolver/shibbolethData';
 export const headers = new HttpHeaders().set('Content-Type', 'application/json');
