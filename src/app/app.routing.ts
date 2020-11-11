2fe802d827260ddaee4892db8ad4f43918fde644
Eike Spielberg <Eike.Spielberg@uni-due.de>
2020-11-11 09:44:54 +0100

    removed jkb

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import {ModuleWithProviders} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-declare module "@angular/core" {
+declare module '@angular/core' {
   interface ModuleWithProviders<T = any> {
     ngModule: Type<T>;
     providers?: Provider[];
@@ -12,7 +12,6 @@ const routes: Routes = [
   {path: 'start', loadChildren: 'app/start/start.module#StartModule'},
   {path: 'reports', loadChildren: 'app/reports/reports.module#ReportsModule'},
   {path: 'counter', loadChildren: 'app/sushi/sushi.module#SushiModule'},
-  {path: 'jkb', loadChildren: 'app/jkb/journal.knowledgebase.module#JournalKnowledgebaseModule'},
   {path: 'ebs', loadChildren: 'app/ebs/ebs.module#EbsModule'}
 ];
 
--- src/app/jkb/journal.knowledgebase.routing.ts
+++
@@ -1,9 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {CollectionManagementComponent} from './collection.management.component';
-
-const routes: Routes = [
-  { path: '', component: CollectionManagementComponent}
-];
-
-export const jkbRouting: ModuleWithProviders = RouterModule.forChild(routes);
